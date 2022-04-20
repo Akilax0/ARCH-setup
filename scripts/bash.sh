fca93d63f6f85d539991736e1d478490ed194c5b
Akilax0 <killerilaksha@gmail.com>
2022-04-20 16:47:48 +0530

    bash: tab completion

---
+++ scripts/bash.sh
@@ -0,0 +1,12 @@
+#bash Auto completion
+sudo apt install bash-completion
+# this creates script for autocompletion @
+/usr/share/bash-completion/bash_completion
+#have to source this file for bashrc
+# run _init_completion to check if not 
+# add line to bashrc
+sourc /usr/share/bash-completion/bash_completion
+
+
+
+
