387b95fd28f24e6899ee8c01765595e8161922bf
Akilax0 <killerilaksha@gmail.com>
2022-12-25 08:11:05 +0530

    tmux pane switching

---
+++ nvim/.config/nvim/after/plugin/toggleterm.lua
@@ -0,0 +1,10 @@
+vim.keymap.set("n", "<leader>t",":ToggleTerm sizem40 diremtion=float <CR>")
+vim.keymap.set("t", "<C-a>","<esc> :ToggleTermToggleAll <CR>")
+vim.keymap.set("t", "<C-q>","exit <CR>")
+vim.keymap.set("n", "<C-a>","ToggleTermAll <CR>")
+
+
+-- nnoremap  <leader>z :ToggleTerm size=40 direction=float <CR>
+-- tmap  <C-z> <C-\><C-n>:ToggleTermToggleAll<CR>
+-- tmap  <C-q> exit<CR>
+-- nmap  <C-z> :ToggleTermToggleAll<CR>
--- nvim/.config/nvim/lua/akilax0/packer.lua
+++ nvim/.config/nvim/lua/akilax0/packer.lua
@@ -43,6 +43,10 @@ return require('packer').startup(function(use)
 	  }
   }
 
+  use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+      require("toggleterm").setup()
+  end}
+
 end)
 
 
